4d62a7e8fbe6085a58753007ed6dc5c1523cde51
Charles Samuels <chasms@chas.ms>
2025-07-01 17:44:19 -0400

    Init

---
+++ README.md
@@ -0,0 +1,25 @@
+# p5 Space
+
+Useful links:
+
+- [p5 Examples](https://p5js.org/examples/)
+- [p5 Documentation (Reference)](https://p5js.org/reference/)
+- [Jolson's Examples](https://jolson-examples.glitch.me/) and [corresponding code]()
+- ML5 Docs:
+  - [General](https://learn.ml5js.org/#/)
+  - [PoseNet](https://learn.ml5js.org/#/reference/posenet)
+
+Bread crumbs:
+
+- JavaScript's class syntax lets you write object-oriented sketches
+- p5.js has a TON of built-in events
+- Use Async / Await to hook into an API and turn this into a data viz project, or let it influence your art more generally
+
+Project Ideas:
+
+- Art / canvas projects - something that lets you draw, create, or mutate images in response to user input.
+
+Unlocked at Part 2:
+
+- Video filters that add shapes (think clown nose on your nose maybe? or moustache?)
+- Video filters that manipulate the image data (e.g. black-and-white image? An image with augmented blue?)
---
+++ p5stars.js
@@ -0,0 +1,53 @@
+let stars = []
+var easyCam;
+
+function setup() {
+  const size = 5000;
+
+  createCanvas(window.innerWidth-20, window.innerHeight-20, WEBGL);
+  setAttributes('antialias', true);
+  
+  easycam = createEasyCam({ distance: size });
+  
+  setupStars(size);
+}
+
+function draw() {
+  background(0);
+  drawEarth();
+
+  stars.forEach(star => star.display());
+}
+
+function setupStars(size) {
+  Array.from(Array(size)).forEach(() => {
+    stars.push(new Star(size));
+  });
+}
+
+function Star(size) {
+  this.size = random(.1, 10);
+  this.x = random(-size, size);
+  this.y = random(-size, size);
+  this.z = random(-size, size);
+  this.color = color(random(0, 255), random(0, 255), random(0, 255), random(25, 100));
+  
+  this.display = function() {
+    push();
+    fill(this.color);
+    stroke(this.color);
+    // noStroke();
+    translate(this.x, this.y, this.z);
+    sphere(this.size);
+    pop();
+  }
+}
+
+function drawEarth() {
+  push();
+  colorMode(HSB);
+  fill(255, 204, 100);
+  noStroke();
+  sphere(10);
+  pop();
+}
